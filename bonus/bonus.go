b76ff807588db66d72a9bb67acc3055325ff509d
kaike.vitorino@sempreceub.com <GEORGESILVA10#@$>
2023-05-28 23:56:54 -0300

    casa

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -6,6 +6,25 @@ import "errors"
 //vai de cidadeAi para cidadeBi. Retorne a cidade de destino, ou seja, a cidade sem nenhum caminho que saia dela.
 
 func Destino(caminhos [][2]string) (string, error) {
-	// Seu código aqui
-	return "", errors.New("Not implemented yet")
+	if len(caminhos) == 0 {
+		return "", errors.New("Não foi possível determinar a cidade de destino")
+	}
+
+	// Crie um mapa para armazenar as cidades de destino e suas ocorrências
+	destinos := make(map[string]int)
+
+	// Percorra todos os caminhos e atualize as ocorrências das cidades de destino
+	for _, caminho := range caminhos {
+		destinos[caminho[1]]++
+		destinos[caminho[0]]--
+	}
+
+	// Encontre a cidade de destino com ocorrência positiva
+	for cidade, ocorrencias := range destinos {
+		if ocorrencias > 0 {
+			return cidade, nil
+		}
+	}
+
+	return "", errors.New("Não foi possível determinar a cidade de destino")
 }
