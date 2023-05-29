89f9f37d4222728896ca7ec19045d3dfe8332033
kaike.vitorino@sempreceub.com <GEORGESILVA10#@$>
2023-05-28 23:43:54 -0300

    casa

--- q4/q4.go
+++ q4/q4.go
@@ -19,5 +19,11 @@ type Student struct {
 }
 
 func UpdateAverage(students map[int]*Student) {
-	// Seu código aqui
+	for _, student := range students {
+		totalGrades := 0.0
+		for _, grade := range student.Grades {
+			totalGrades += grade
+		}
+		student.Average = totalGrades / float64(len(student.Grades))
+	}
 }
